66ccb7d458cb0fc2093c08ca3662f58833eb467d
kevinhelly <kevinhelly.kh@gmail.com>
2014-10-20 11:52:33 +0200

    Created Unit Test

--- TentamenOOAD/CrimeHeadlineSystem.Tests/UnitTest1.cs
+++ TentamenOOAD/CrimeHeadlineSystem.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CrimeHeadlineSystem.Classes.Factory;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CrimeHeadlineSystem.Tests
@@ -7,8 +8,18 @@ namespace CrimeHeadlineSystem.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_If_Screen_Scraper_Returns_Value()
         {
+            var expectedPolisenNews = "Så kan Polisen bättre utreda våldtäkter";
+            var expectedUtryckningNews = "Brand på Ahlstroms i Ställdalen";
+
+            var polisen = ScrapingFactory.CreateScreenScraperForPolisen();
+            var polisenNews = polisen.GetHtmlContentFromScraping();
+            var utryckning = ScrapingFactory.CreateScreenScraperForUtryckning();
+            var utryckningNews = utryckning.GetHtmlContentFromScraping();
+
+            Assert.AreEqual(expectedPolisenNews, polisenNews);
+            Assert.AreEqual(expectedUtryckningNews, utryckningNews);
         }
     }
 }
